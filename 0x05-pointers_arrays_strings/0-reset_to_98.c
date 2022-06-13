d4ed76bbdbe529d259b18215327cfe8cc0c53aba
kellyxglobal <kellyxglobal@gmail.com>
2022-06-13 13:45:33 -0700

    0-reset

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * reset_to_98(int *n) - check the code
  *
- * Return: Always 0.
+ * Return: Always n;
  */
 void reset_to_98(int *n)
 {
 	int num = 98;
 	*n = num;
+	return (0);
 }
