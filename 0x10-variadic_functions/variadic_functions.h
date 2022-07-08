16de91daa1da852b9990085a3bc11d7e486e6d5a
kellyxglobal <kellyxglobal@gmail.com>
2022-07-08 16:16:35 -0700

    prototypes of all functions and the prototype of the function _putchar

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,21 +1,12 @@
-#ifndef VARIADIC_FUNCTIONS_H
-#define VARIADIC_FUNCTIONS_H
-#include <stdarg.h>
+#ifndef _VARIADIC_FUNCTIONS_
+#define _VARIADIC_FUNCTIONS_
 
-int _putchar(char c);
+#include <stdarg.h>
+#include <stdio.h>
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsined int n, ...);
-void print_all(const char *const format, ...);
-void print_a_char(char *separator, va_list args);
-void print_a_integer(char *separator, va_list args);
-void print_a_float(char *separator, va_list args);
-void print_a_char_ptr(char *separator, va_list args);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
 
-typedef struct format_types
-{
-  char *type;
-  void (*f)();
-} checker;
 #endif
-
