8ba46f0634bf9d57862d8f4d73e933168e8c1f6f
kellyxglobal <Kellyxglobal@gmail.com>
2022-07-16 12:05:06 -0700

    A program to print lsit of timestable

---
+++ 0x02-functions_nested_loops/practice_folder/times_table.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 0; i++)
+	{
+		for (j = 0; j < 0; j++)
+		{
+			k = i * j;
+			if (j == 0)
+			{
+				putchar(k + '0');
+			}
+
+			if (k < 10 && j != 0)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar(' ');
+				putchar(k + '0');
+			}
+			else if (k >= 10)
+			{
+				putchar(',');
+				putchar(' ');
+				putchar((k / 10) + '0');
+				putchar((k % 10) + '0');
+			}
+		}
+		putchar('\n');
+	}
+}
+
